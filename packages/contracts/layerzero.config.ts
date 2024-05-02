650592ec4a14e11ee6ad859fe89414ebba4dd396
cuonghx-gutech <cuonghx@gu-tech.com>
2024-05-02 10:33:42 +0700

    Deployed fixedrate contract

--- packages/contracts/layerzero.config.ts
+++ packages/contracts/layerzero.config.ts
@@ -3,12 +3,12 @@ import { EndpointId } from '@layerzerolabs/lz-definitions'
 
 const sepoliaContract = {
     eid: EndpointId.SEPOLIA_V2_TESTNET,
-    contractName: 'MyOFT',
+    contractName: 'FixedRate',
 }
 
 const joctContract = {
     eid: EndpointId.JOC_V2_TESTNET,
-    contractName: 'MyNativeOFT',
+    contractName: 'FixedRate',
 }
 
 export default {
